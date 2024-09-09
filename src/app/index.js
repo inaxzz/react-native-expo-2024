8595725c844a22444a5a8de186c95cfd715b052f
Inácio da Silva Rodrigues <161024926+inaxzz@users.noreply.github.com>
2024-09-09 19:02:48 +0000

    [feat] PagerView in the Home

--- src/app/index.js
+++ src/app/index.js
@@ -64,7 +64,7 @@ export default function App() {
 
       <Button
         style={styles.button}
-        title="Entrar"
+               title="Entrar"
         onPress={handleEntrarSuper}
       />
 
